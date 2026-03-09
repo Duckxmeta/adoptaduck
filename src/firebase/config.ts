6595c03c23c65913ac2ef9485e154337ee7ee6fa
Duckxmeta <flowmarket1@gmail.com>
2026-03-09 22:35:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7482167027-804c1",
+  "appId": "1:273815600873:web:ce271ffc3b0e025210ba78",
+  "apiKey": "AIzaSyDfUdHuBLDaJiaLBBjbMZ6LbPevGTYpRLs",
+  "authDomain": "studio-7482167027-804c1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "273815600873"
+};
