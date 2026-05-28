eded258289bf66557f60d2739129a22b9cd05fb4
Duckxmeta <flowmarket1@gmail.com>
2026-05-28 16:15:35 -0500

    fix: define storageBucket in Firebase configuration

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,8 @@
 import firebaseConfigImport from '../../firebase-applet-config.json';
 
-export const firebaseConfig = firebaseConfigImport;
+export const firebaseConfig = {
+  ...firebaseConfigImport,
+  storageBucket:
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
+    "studio-7482167027-804c1.firebasestorage.app"
+};
